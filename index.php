59149b01d021a2628d35565f5f45c696d8320a6b
brunaandrade6586 <bruna.6586537@aluno.mg.gov.br>
2024-12-05 08:36:48 -0300

    prova

---
+++ index.php
@@ -0,0 +1,28 @@
+<h2>receita:
+    <?php
+    echo "pão com mortadela"
+    ?>
+    </h2>
+    <table border="1">
+        <thead>
+            <tr>
+                <th>ingredientes</th>
+                <th>quantidade</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>pão</td>
+                <td> um </td>
+            </tr>
+           <tr>
+            <td>mortadela</td>
+            <td>um</td>
+        </tr>
+          </tbody>
+      </table>
+      <h1> nota: </h1>
+<script>
+    var h1 = document.querySelector("h1")
+    h1.innerHTML="nota:5"
+</script>
